e64733aa4b7e1d49f8b21576d67b25c3c749379d
pbx-profi <33713952+pbx-profi@users.noreply.github.com>
2022-06-22 14:00:15 +0300

    Update bulk-extensions-gen.php

--- bulk-extensions-gen.php
+++ bulk-extensions-gen.php
@@ -6,12 +6,13 @@
 
 $first = "101";
 $last = "110";
+$tech = "pjsip"
 $password = "REGEN";
 
 echo 'extension,name,description,tech,secret,callwaiting_enable,findmefollow_enabled,findmefollow_grplist,voicemail'."\n";
 
 for($a=$first;$a<$last;$a++) {
-        echo "$a,$a,$a,pjsip,$password,,,$a,novm\n";
+        echo "$a,$a,$a,$tech,$password,,,$a,novm\n";
 
 }
 ?>
